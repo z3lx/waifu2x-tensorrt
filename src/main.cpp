0e291766aa2e32424bd6f3f26527accbaa26f275
z3lx <z3lx@pm.me>
2023-08-26 20:24:05 -0400

    Add support for building TensorRT model from ONNX model

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
+#include "tensorrt/engine.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string onnxModelPath;
+    int maxBatchSize;
+    int maxSize;
+
+    std::cout << "Enter ONNX model path: ";
+    std::cin >> onnxModelPath;
+    std::cout << "Enter max batch size: ";
+    std::cin >> maxBatchSize;
+    std::cout << "Enter max size: ";
+    std::cin >> maxSize;
+
+    trt::Config config;
+    config.maxBatchSize = maxBatchSize;
+    config.maxWidth = maxSize;
+    config.maxHeight = maxSize;
+    trt::Engine engine(config);
+    engine.build(onnxModelPath);
     return 0;
 }
---
+++ src/tensorrt/logger.h
@@ -0,0 +1,16 @@
+#ifndef WAIFU2X_TENSORRT_TRT_LOGGER_H
+#define WAIFU2X_TENSORRT_TRT_LOGGER_H
+
+#include <iostream>
+#include <NvInferRuntimeBase.h>
+
+namespace trt {
+    class Logger : public nvinfer1::ILogger {
+    public:
+        Logger() = default;
+        virtual ~Logger() = default;
+        void log (Severity severity, const char* msg) noexcept override;
+    };
+}
+
+#endif //WAIFU2X_TENSORRT_TRT_LOGGER_H
