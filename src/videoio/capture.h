7971d1ffc3d5c89a9dca2082141b5188ed95e180
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-13 08:56:35 -0400

    Add properties getters to ffmpeg video capture

--- src/videoio/capture.h
+++ src/videoio/capture.h
@@ -5,20 +5,29 @@
 
 class VideoCapture {
 public:
-    VideoCapture();
-    explicit VideoCapture(std::string ffmpegDir);
+    VideoCapture() noexcept;
     virtual ~VideoCapture();
     void open(const std::string& path);
-    bool isOpened() const;
-    void read(cv::Mat& frame);
+    [[nodiscard]] bool isOpened() const noexcept;
+    bool read(cv::Mat& frame);
     void release();
 
+    // region Getters
+    [[nodiscard]] const std::string& getFfmpegDir() const noexcept;
+    [[nodiscard]] const cv::Size2i& getFrameSize() const noexcept;
+    [[nodiscard]] double getFrameRate() const noexcept;
+    [[nodiscard]] int getFrameCount() const noexcept;
+    [[nodiscard]] int getFrameIndex() const noexcept;
+    // endregion
 private:
+    FILE* pipe = nullptr;
+    bool opened = false;
+
     std::string ffmpegDir;
-    FILE* pipe;
-    int frameWidth;
-    int frameHeight;
-    bool opened;
+    cv::Size2i frameSize = cv::Size2i(-1, -1);
+    double frameRate = -1;
+    int frameCount = -1;
+    int frameIndex = -1;
 };
 
 #endif //WAIFU2X_TENSORRT_VIDEOIO_CAPTURE_H
