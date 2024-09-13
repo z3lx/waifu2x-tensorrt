05d4db55bda6258d6ce452b6ad743b69eb3aa2eb
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-13 08:56:34 -0400

    Improve logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,40 @@
-#include "tensorrt/img2img.h"
+#include <iostream>
 #include <opencv2/cudaimgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <plog/Formatters/TxtFormatter.h>
-#include <plog/Log.h>
-#include <plog/Init.h>
-#include <plog/Appenders/ColorConsoleAppender.h>
+#include "tensorrt/img2img.h"
+
+#define SPDLOG_LEVEL_NAMES { "TRACE", "DEBUG", "INFO ", "WARN ", "ERROR", "FATAL", "OFF" }
+#define SPDLOG_NO_NAME
+#include <spdlog/spdlog.h>
 
 int main() {
-    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
-    plog::init(plog::debug, &consoleAppender);
+    auto console = spdlog::stdout_color_mt("console");
+    console->set_level(spdlog::level::debug);
+    console->set_pattern("[%H:%M:%S.%e] [%^%l%$] %v");
+
+    trt::LogCallback callback = [&console](trt::Severity severity, const std::string& message, const std::string& file, const std::string& function, int line) {
+        std::string s = "[" + function + "@" + std::to_string(line) + "] " + message;
+        switch (severity) {
+            case trt::Severity::critical:
+                console->critical(s);
+                break;
+            case trt::Severity::error:
+                console->error(s);
+                break;
+            case trt::Severity::warn:
+                console->warn(s);
+                break;
+            case trt::Severity::info:
+                console->info(s);
+                break;
+            case trt::Severity::debug:
+                console->debug(s);
+                break;
+            case trt::Severity::trace:
+                console->trace(s);
+                break;
+        }
+    };
 
     std::string imageDir = R"(C:\waifu2x-tensorrt\images\)";
     std::string imagePath = imageDir + "bg.png";
@@ -19,9 +44,9 @@ int main() {
     int batchSize = 1;
     int tileSize = 256;
     cv::Point2i scaling(4, 4);
-    cv::Point2d overlap(0.125f, 0.125f);
+    cv::Point2d overlap(0.125, 0.125);
 
-    int iterations = 100;
+    int iterations = 1;
 
     trt::RenderConfig renderConfig;
     renderConfig.deviceId = deviceId;
@@ -34,6 +59,7 @@ int main() {
     renderConfig.overlap = overlap;
 
     trt::Img2Img engine;
+    engine.setLogCallback(callback);
     engine.load(modelPath, renderConfig);
 
     cv::Mat input = cv::imread(imagePath);
@@ -50,14 +76,12 @@ int main() {
         engine.render(gpuInput, gpuOutput);
         tm.stop();
     }
-    std::cout << "Total time: " << tm.getTimeMilli() << std::endl;
-    std::cout << "Average time: " << tm.getTimeMilli() / iterations << std::endl;
+    console->info("Total time: {}", tm.getTimeMilli());
+    console->info("Average time: {}", tm.getTimeMilli() / iterations);
 
     cv::cuda::cvtColor(gpuOutput, gpuOutput, cv::COLOR_RGB2BGR);
     gpuOutput.download(output);
     cv::imwrite(imageDir + "out.png", output);
-
-    system("pause");
 
     return 0;
 }
--- src/tensorrt/logger.h
+++ src/tensorrt/logger.h
@@ -1,35 +1,33 @@
 #ifndef WAIFU2X_TENSORRT_TRT_LOGGER_H
 #define WAIFU2X_TENSORRT_TRT_LOGGER_H
 
-#include <iostream>
+#include <functional>
+#include <string>
 #include <NvInferRuntimeBase.h>
-#include <plog/Log.h>
-#include <plog/Severity.h>
 
 namespace trt {
+    enum Severity {
+        critical,
+        error,
+        warn,
+        info,
+        debug,
+        trace
+    };
+
+    using LogCallback = std::function<void(trt::Severity, const std::string&, const std::string&, const std::string&, int)>;
+
     class Logger : public nvinfer1::ILogger {
     public:
-        Logger() = default;
-        virtual ~Logger() = default;
-        void log (Severity severity, const char* msg) noexcept override {
-            switch (severity) {
-                case Severity::kINTERNAL_ERROR:
-                    PLOG(plog::fatal) << msg;
-                    break;
-                case Severity::kERROR:
-                    PLOG(plog::error) << msg;
-                    break;
-                case Severity::kWARNING:
-                    PLOG(plog::warning) << msg;
-                    break;
-                case Severity::kINFO:
-                    PLOG(plog::info) << msg;
-                    break;
-                case Severity::kVERBOSE:
-                    PLOG(plog::verbose) << msg;
-                    break;
-            }
-        }
+        Logger();
+        ~Logger() override;
+
+        void setLogCallback(LogCallback callback);
+        void log(trt::Severity severity, const std::string& message, const std::string& file, const std::string& function, int line);
+        void log(Severity severity, const char* msg) noexcept override;
+
+    private:
+        LogCallback logCallback{};
     };
 }
 
