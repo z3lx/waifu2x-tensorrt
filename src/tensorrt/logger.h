517064461802b105af304e864c961a09f227bae4
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-13 08:56:35 -0400

    Add progress callback

--- src/tensorrt/logger.h
+++ src/tensorrt/logger.h
@@ -5,7 +5,7 @@
 #include <functional>
 #include <string>
 
-#define LOG(severity, message) log(severity, message, __FILE__, __FUNCTION__, __LINE__)
+#define LOG(severity, message) log(severity, message, __FUNCTION__, __LINE__)
 
 namespace trt {
     enum Severity {
@@ -17,19 +17,25 @@ namespace trt {
         trace
     };
 
-    using LogCallback = std::function<void(trt::Severity, const std::string&, const std::string&, const std::string&, int)>;
+    using MessageCallback = std::function<void(trt::Severity, const std::string&)>;
+    using ProgressCallback = std::function<void(int, int, double)>;
 
     class Logger : public nvinfer1::ILogger {
     public:
         Logger();
         ~Logger() override;
 
-        void setLogCallback(LogCallback callback);
-        void log(trt::Severity severity, const std::string& message, const std::string& file, const std::string& function, int line);
+        void setMessageCallback(MessageCallback callback);
+        void setProgressCallback(ProgressCallback callback);
+
+        void log(trt::Severity severity, const std::string& message);
+        void log(trt::Severity severity, const std::string& message, const std::string& function, int line);
         void log(ILogger::Severity severity, const char* msg) noexcept override;
+        void log(int current, int total, double speed);
 
     private:
-        LogCallback logCallback{};
+        MessageCallback messageCallback{};
+        ProgressCallback progressCallback{};
     };
 }
 
